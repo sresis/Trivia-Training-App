0a9389fd6e66a845438a90b2eaf14082e2e277aa
Stephanie Resis <sresis@umich.edu>
2020-10-30 15:20:18 -0700

    route to questions page

--- src/App.js
+++ src/App.js
@@ -1,15 +1,24 @@
 import './App.css';
 import Questions from './Questions';
-import { Router, Route, browserHistory, IndexRoute, Redirect  } from 'react-router'
+import { Route, Switch, BrowserRouter as Router, IndexRoute, Redirect  } from 'react-router-dom'
 import { Container, Row, Col, ListGroup, Button, Modal } from 'react-bootstrap';
+import React, { Component, useState } from "react";
+
+import Home from './Home';
 
 function App() {
+  const [start, setStart] = React.useState(false);
   return (
-    <div className="App">
-      <Button>Play Game</Button>
-      < Questions/>
-    </div>
-  );
+    <Router>
+      <Switch>
+        <Route path="/questions" component={Questions}></Route>
+        <Route path="/" component={Home}></Route>
+
+      </Switch>
+      
+
+    </Router>
+  )
 }
 
 export default App;
--- src/Questions.js
+++ src/Questions.js
@@ -2,13 +2,15 @@ import React, { Component, useState } from "react";
 import { Col, Container, Nav, Navbar, Row, Button } from 'react-bootstrap';
 import triviaQuestions from "./Data.json";
 function Questions() {
-    // store the incorrect and correct answers in an array
+    // store the incorrect and correct answers together in an array
     for (const item of triviaQuestions) {
-        const allOptions = item.incorrect;
-        allOptions.push(item.correct);
-        console.log(allOptions)
-
+        let options = item.incorrect;
+        options.push(item.correct);
+        item.allOptions = options;
     }
+    const values = Object.values(triviaQuestions)
+    const randomValue = values[parseInt(Math.random() * values.length)]
+    console.log(randomValue)
 
         // show one question
     return(
